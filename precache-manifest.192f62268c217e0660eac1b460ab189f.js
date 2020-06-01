21c5118ea1d636533b1da54ab4b1c27d58c7f3b4
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-06-01 18:27:12 +0300

    Updates

---
+++ precache-manifest.192f62268c217e0660eac1b460ab189f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5887203a220881407aa0eafc9a0b2a9e",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "1f8a26f19ba6e848caa2",
+    "url": "/dad-jokes/static/css/main.07ffb950.chunk.css"
+  },
+  {
+    "revision": "4b7202f9429a665194ac",
+    "url": "/dad-jokes/static/js/2.733d6214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes/static/js/2.733d6214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8a26f19ba6e848caa2",
+    "url": "/dad-jokes/static/js/main.e07c9806.chunk.js"
+  },
+  {
+    "revision": "6c62c91ed167010bf8c9",
+    "url": "/dad-jokes/static/js/runtime-main.949c1f6c.js"
+  }
+]);
